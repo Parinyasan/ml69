035de8323a7a9a2b8150aed7d98d90ab02965636
Parinya Sanguansat <36033770+Parinyasan@users.noreply.github.com>
2026-09-11 10:21:06 +0700

    Update target organizations in update_data.py

--- update_data.py
+++ update_data.py
@@ -10,7 +10,7 @@ auth = Auth.Token(token)
 g = Github(auth=auth)
 
 # ⚠️ อาจารย์อย่าลืมเปลี่ยนชื่อ Org ด้านล่างให้ตรงกับของนักศึกษาจริงนะครับ
-target_orgs = ["company-a-org", "company-b-org"] 
+target_orgs = ["finoria-co", "ideamee", "vantage-consortium-co-ltd"] 
 
 dashboard_data = {}
 
